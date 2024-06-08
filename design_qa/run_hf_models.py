a8d7d537ceb1cfb653af7fafc7373f91ec97e318
anniedoris <anniecd88@gmail.com>
2024-06-08 15:03:19 -0400

    able to run llama 2 and llama 2 chat on mac

--- design_qa/run_hf_models.py
+++ design_qa/run_hf_models.py
@@ -3,6 +3,8 @@ from transformers import AutoTokenizer
 import sys
 sys.path.append("utils")
 from inference import *
+import transformers
+import torch
 
 
 # result = inference_llm("NousResearch/Llama-2-7b-chat-hf", "What is your name?", cuda=False)
@@ -14,3 +16,48 @@ from inference import *
 # tokenizer = AutoTokenizer.from_pretrained("lmsys/vicuna-7b-v1.5", padding_side="left")
 # model_inputs = tokenizer(["What is your name"], return_tensors="pt")
 
+# Inference pipeline for llama 2 chat
+model = "NousResearch/Llama-2-7b-chat-hf"
+
+tokenizer = AutoTokenizer.from_pretrained(model)
+
+model = AutoModelForCausalLM.from_pretrained(model)
+
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    tokenizer = tokenizer,
+    torch_dtype=torch.float16,
+    max_new_tokens = 512,
+    device_map="auto",
+)
+
+sequences = pipeline(
+    "I'm looking to make a lemon cake. Could you please give me a recipe for a good lemon cake?",
+    do_sample=True,
+)
+
+print(sequences[0].get("generated_text"))
+
+# Inference pipeline for llama 2 no chat
+model = "NousResearch/Llama-2-7b-hf"
+
+tokenizer = AutoTokenizer.from_pretrained(model)
+
+model = AutoModelForCausalLM.from_pretrained(model)
+
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    tokenizer = tokenizer,
+    torch_dtype=torch.float16,
+    max_new_tokens = 512,
+    device_map="auto",
+)
+
+sequences = pipeline(
+    "I'm looking to make a lemon cake. Could you please give me a recipe for a good lemon cake?",
+    do_sample=True,
+)
+
+print(sequences[0].get("generated_text"))
