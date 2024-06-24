1d324faf1692eac510448a699922a251ec8c2871
adoris <adoris@mit.edu>
2024-06-24 10:58:23 -0400

    small edit to metrics to deal with numeric characters, convert them to strings

--- design_qa/metrics.py
+++ design_qa/metrics.py
@@ -55,7 +55,7 @@ def normalize_answer(s):
         return "".join(ch for ch in text if ch not in exclude)
 
     def lower(text):
-        return text.lower()
+        return str(text).lower()
 
     return white_space_fix(remove_articles(remove_punc(lower(s))))
 
