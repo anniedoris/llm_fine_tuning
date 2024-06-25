dbfab0e546e3f75ffb9eed16f1854ec88db9775b
adoris <adoris@mit.edu>
2024-06-25 14:50:05 -0400

    removing a bunch of deleted files and adding utils

--- utils/hf_dataset_upload.py
+++ utils/hf_dataset_upload.py
@@ -1,6 +1,8 @@
 from datasets import load_dataset
 import sys
 sys.path.append("design_qa")
+sys.path.append("datasets")
 
-dataset = load_dataset('csv', data_files='design_qa/context_and_prompts.csv')
-dataset.push_to_hub("designqa_retrieval_with_context", private=True)
+# Uploading a dataset to the hub
+dataset = load_dataset('csv', data_files='datasets/rule_retrieval_qa.csv')
+dataset.push_to_hub("designqa_retrieval")
